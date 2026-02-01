4c6c6bfc36aa0634d109805dc90a84f150192fc4
Eurrieta Eyluimar (Producto y Tecnologia) <eeurrieta@lanacion.com.ar>
2026-02-01 20:30:03 -0300

    feat: add API constants and configuration

---
+++ src/constants/api.ts
@@ -0,0 +1,3 @@
+export const API_BASE_URL = 'https://rickandmortyapi.com/api';
+
+export const CHARACTERS_PER_PAGE = 6;
---
+++ src/constants/site.ts
@@ -0,0 +1,9 @@
+export const SITE = {
+  title: 'Rick and Morty - Explora el multiverso episodio por episodio',
+  description:
+    'Explora y compara personajes de Rick and Morty. Descubre sus episodios exclusivos y compartidos con esta herramienta interactiva.',
+  author: 'Rick and Morty Challenge',
+  themeColor: '#0f172a',
+  language: 'es',
+  apiUrl: 'https://rickandmortyapi.com/',
+};
