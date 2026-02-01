f415f5579ee8843d219c5fccc2f4654f6902f70b
Eurrieta Eyluimar (Producto y Tecnologia) <eeurrieta@lanacion.com.ar>
2026-02-01 20:39:08 -0300

    test: configure Jest with React Testing Library

---
+++ jest.config.js
@@ -0,0 +1,25 @@
+const nextJest = require('next/jest')
+
+const createJestConfig = nextJest({
+  dir: './',
+})
+
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testEnvironment: 'jest-environment-jsdom',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  testMatch: [
+    '**/__tests__/**/*.test.[jt]s?(x)',
+    '**/?(*.)+(spec|test).[jt]s?(x)',
+  ],
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/**/__tests__/**',
+    '!src/**/*.stories.{js,jsx,ts,tsx}',
+  ],
+}
+
+module.exports = createJestConfig(customJestConfig)
